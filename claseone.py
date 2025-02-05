4b86827be58ae3174e176c36ec67543ab3be9af0
MoncadaVK <149018251+MoncadaVK@users.noreply.github.com>
2025-02-05 17:43:10 -0600

    f

---
+++ claseone.py
@@ -0,0 +1,51 @@
+saldo=2000
+nombre="axel"
+ 
+iva=0.200
+
+print('hola')
+print("amigo")
+
+
+total= saldo-iva
+#creamos una variable a la cual le restamos 
+#el iva de la variable saldo
+
+#El comentario se coloca
+#con el simbolo de numeral #
+valor=2
+
+
+#El valor reservado en memoria llamado def 
+#se utiliza para definir una función 
+
+#sintaxis de una función: def nombre_funcion():
+def mostrar_informacion():
+    #A este proseso de llamar valores 
+
+    #Tambien se indentifica por 
+    #la sangria que se coloca
+    #al inicio de la linea de codigo
+    #de color gris 
+
+
+    #Dentro de la funcion colocare el valor
+    #print para mostrar la información de varables
+    #anteriores
+    print(f"Nombre: {nombre}")
+    print(f"Saldo: {saldo}")
+    print(f"Iva: {iva}")
+    print(f"Total: {total}")
+
+# Llamar a la función para mostrar la información
+mostrar_informacion
+
+
+fruta=('manzana')
+print(fruta)
+
+
+
+fruta=('manzana' , 'pera', 'uva', 'sandia')
+print(fruta)
+#
